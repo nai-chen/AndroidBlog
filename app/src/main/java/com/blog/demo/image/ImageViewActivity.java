c26bcec51c675725ae5fa50bc565cdec6a3a88bb
nai-chen <chennai1101@sohu.com>
2019-01-26 15:02:26 +0800

    ImageView

---
+++ app/src/main/java/com/blog/demo/image/ImageViewActivity.java
@@ -0,0 +1,16 @@
+package com.blog.demo.image;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.blog.demo.R;
+
+public class ImageViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_image_view);
+    }
+}
