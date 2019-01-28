80bfb62c1c086ec66615fec089e3e97751a05cfc
nai-chen <41614350+nai-chen@users.noreply.github.com>
2019-01-28 17:20:47 +0800

    Selector

--- app/src/main/java/com/blog/demo/image/SelectorActivity.java
+++ app/src/main/java/com/blog/demo/image/SelectorActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.blog.demo.R;
+
 public class SelectorActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout);
+        setContentView(R.layout.activity_image_selector);
     }
+
 }
