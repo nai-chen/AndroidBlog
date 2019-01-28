5baef4a6499c4032867679607cbac8c7cd678bf2
nai-chen <41614350+nai-chen@users.noreply.github.com>
2019-01-28 14:48:09 +0800

    Shape

---
+++ app/src/main/java/com/blog/demo/image/SelectorActivity.java
@@ -0,0 +1,15 @@
+package com.blog.demo.image;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SelectorActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout);
+    }
+}
