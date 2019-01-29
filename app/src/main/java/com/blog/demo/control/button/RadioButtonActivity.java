b48b7646d356e24b5ea313509810b245e96a8f30
nai-chen <chennai1101@sohu.com>
2019-01-29 17:57:11 +0800

    button

---
+++ app/src/main/java/com/blog/demo/control/button/RadioButtonActivity.java
@@ -0,0 +1,16 @@
+package com.blog.demo.control.button;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.blog.demo.R;
+
+public class RadioButtonActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_button_radio);
+    }
+}
---
+++ app/src/main/java/com/blog/demo/control/button/SwitchActivity.java
@@ -0,0 +1,15 @@
+package com.blog.demo.control.button;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.blog.demo.R;
+
+public class SwitchActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_button_switch);
+    }
+}
