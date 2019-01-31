6c9f50cbb80db1ec3420ab75bed7e72a2d56a2ef
nai-chen <41614350+nai-chen@users.noreply.github.com>
2019-01-31 08:33:54 +0800

    modify

--- app/src/main/java/com/blog/demo/control/layout/RelativeLayoutAlignParentActivity.java
+++ app/src/main/java/com/blog/demo/control/layout/RelativeLayoutAlignParentActivity.java
@@ -13,4 +13,5 @@ public class RelativeLayoutAlignParentActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_layout_relative_align_parent);
     }
+
 }
