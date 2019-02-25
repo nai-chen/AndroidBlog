5a6ed1f4a9a9e2e1de0a4b54917d50ad226d1e28
nai-chen <41614350+nai-chen@users.noreply.github.com>
2019-02-25 18:01:14 +0800

    Canvas

--- app/src/main/java/com/blog/demo/image/canvas/CanvasTranslationActivity.java
+++ app/src/main/java/com/blog/demo/image/canvas/CanvasTranslationActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -20,7 +21,7 @@ public class CanvasTranslationActivity extends Activity {
     }
 
     class CanvasTranslationView extends View {
-        private Paint mRedPaint, mBluePaint;
+        private Paint mRedPaint, mBluePaint, mGreenPaint;
 
         public CanvasTranslationView(Context context) {
             this(context, null);
@@ -36,18 +37,59 @@ public class CanvasTranslationActivity extends Activity {
 
             mBluePaint = new Paint(mRedPaint);
             mBluePaint.setColor(Color.BLUE);
+            mBluePaint.setAlpha(100);
+
+            mGreenPaint = new Paint(mRedPaint);
+            mGreenPaint.setColor(Color.GREEN);
+            mGreenPaint.setAlpha(50);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
+            Rect rect = new Rect(0, 0, 250, 100);
 
-            canvas.drawRect(50, 50, 300, 200, mRedPaint);
             canvas.save();
+            canvas.translate(100, 100);
+            canvas.drawRect(rect, mRedPaint);
             canvas.translate(50, 50);
-            canvas.drawRect(50, 50, 300, 200, mBluePaint);
+            canvas.drawRect(rect, mBluePaint);
             canvas.restore();
 
+            canvas.save();
+            canvas.translate(600, 100);
+            canvas.drawRect(rect, mRedPaint);
+            canvas.rotate(45);
+            canvas.drawRect(rect, mBluePaint);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(600, 100);
+            canvas.rotate(90, 125, 50);
+            canvas.drawRect(rect, mGreenPaint);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(100, 500);
+            canvas.drawRect(rect, mRedPaint);
+            canvas.skew(1, 0);
+            canvas.drawRect(rect, mBluePaint);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(100, 500);
+            canvas.skew(0, 1);
+            canvas.drawRect(rect, mGreenPaint);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(600, 500);
+            canvas.drawRect(rect, mRedPaint);
+            canvas.scale(0.5f, 2f);
+            canvas.drawRect(rect, mBluePaint);
+            canvas.restore();
         }
+
     }
+
+
 }
