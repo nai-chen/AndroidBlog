ce0a5add3aa49034280e980d9de7de9b184e7aab
nai-chen <41614350+nai-chen@users.noreply.github.com>
2019-12-31 18:05:56 +0800

    navigation view

--- app/src/main/java/com/blog/demo/md/NavigationViewActivity.java
+++ app/src/main/java/com/blog/demo/md/NavigationViewActivity.java
@@ -2,7 +2,12 @@ package com.blog.demo.md;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.blog.demo.R;
 
@@ -13,6 +18,23 @@ public class NavigationViewActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_material_design_navigation_view);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.menu_address
+                        || menuItem.getItemId() == R.id.menu_more) {
+                    menuItem.setChecked(true);
+                }
+                Toast.makeText(NavigationViewActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                menuItem.getTitle();
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
 }
